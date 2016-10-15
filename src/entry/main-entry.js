fd7fc7ae52abe7d2e03f56e7cbf672b27282da72
wangfei25 <wangfei25@meituan.com>
2016-10-15 20:45:58 +0800

    获取input及submit demo

--- src/entry/main-entry.js
+++ src/entry/main-entry.js
@@ -3,3 +3,11 @@ $('#dialog').on('click', () => {
     $('#myModal').modal('show');
 }
 );
+
+
+$('#submit').on('click', () => {
+    // jQuery ajax调用可以放在这里
+    // 获取参数比如
+   var name =  $('#name').val();   //
+   console.log(name);
+});
